967cc723a7da76b4d7648e8fb22bae016d09464a
Breno2840 <128945701+Breno2840@users.noreply.github.com>
2025-10-16 18:50:05 -0300

    Atualizar o radio_station.dart

--- lib/models/radio_station.dart
+++ lib/models/radio_station.dart
@@ -2,11 +2,13 @@ class RadioStation {
   final String name;
   final String streamUrl;
   final String? artUrl;
+  final String? location; // NOVO!
 
   RadioStation({
     required this.name,
     required this.streamUrl,
     this.artUrl,
+    this.location, // NOVO!
   });
 
   factory RadioStation.fromJson(Map<String, dynamic> json) {
@@ -17,6 +19,7 @@ class RadioStation {
       name: json['name'] ?? json['title'] ?? 'Unknown Station',
       streamUrl: streamUrl,
       artUrl: json['artUrl'] ?? json['image'] ?? json['logo'],
+      location: json['location'] ?? json['city'] ?? json['state'] ?? json['country'], // NOVO!
     );
   }
 }
